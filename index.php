5a26ad04834b50287f6acba7d4c2cf0909f1923a
Mara Um <mara.um@gmail.com>
2022-04-28 16:35:43 +0200

    Fin de journée

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'php_parts/defines.php';
+include_once 'php_parts/functions.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Hackers_Poulette</title>
+    <link rel="stylesheet" href="stylesheet/main.css">
+</head>
+<body>
+    <?php include 'php_parts/honeypot.php';?>
+    <main>
+    <?php
+    include 'php_parts/header.php';
+    include 'php_parts/formulaire.php';
+    ?>
+
+    </main>
+</body>
+</html>
